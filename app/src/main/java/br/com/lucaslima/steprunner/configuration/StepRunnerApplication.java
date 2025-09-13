35361ec0742d94f11b2c64676086999617ebe33b
Lucas Lima <lucaslima_miml@hotmail.com>
2025-09-13 14:12:55 -0300

    initialize project structure with Gradle, Spring Boot, and essential configuration files

---
+++ app/src/main/java/br/com/lucaslima/steprunner/configuration/StepRunnerApplication.java
@@ -0,0 +1,13 @@
+package br.com.lucaslima.steprunner.configuration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StepRunnerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StepRunnerApplication.class, args);
+    }
+
+}
